cac53eb19a03357803ec486daea3d4fd3a89bc7a
JM0501 <105787557+JM0501@users.noreply.github.com>
2024-02-24 21:32:16 +0200

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,37 @@
+@ECHO OFF
+REM Mr. T. Mohlamonyane
+REM Computer Science 2A 2024
+REM Practical 01
+REM Set path for JDK
+SET JAVA_HOME="C:\jdk-21"
+SET PATH=%JAVA_HOME%\bin;%PATH%;
+
+REM Display Java version.
+javac --version
+
+REM set path variables...
+cd ..
+set PRAC_BIN=.\bin
+set PRAC_SRC=.\src
+set PRAC_DOC=.\docs
+
+REM clean all class files in the folder.
+del %PRAC_BIN%\*.class
+
+REM Code compilation
+ECHO Trying Code Compilation
+javac -sourcepath %PRAC_SRC% -cp %PRAC_BIN% -d %PRAC_BIN% %PRAC_SRC%\*.java
+
+REM set the maximum of the array to be reversed.
+SET LIMIT=8
+
+REM Running Code
+ECHO Trying to run
+REM package accse.csc2a
+java -classpath %PRAC_BIN% Main %LIMIT%
+
+
+REM Go back to docs folder
+cd %PRAC_DOC%
+
+PAUSE
